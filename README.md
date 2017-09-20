19b65ccae4c7aa3f67c3cdb820c541da0ce0199b
Jet Fontanilla <jet.fontanilla@englishcentral.com>
2017-09-20 14:57:22 -0400

    implement TypeScript wrapper for enumerateDevices()

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# select-microphone-camera-typescript
-TypeScript version of getUserMedia() to select and set Microphone/Webcam
+# Select Microphone/Camera in TypeScript
+
+Use this class for selecting and setting preferred Microphone/Webcam for media capture
+
+This is a simple TypeScript wrapper for enumerateDevices(), storing the preferred media device, and passing it as a constraint to getUserMedia()
---
+++ examples/basic-usage.ts
@@ -0,0 +1,34 @@
+import { BrowserMediaDevice } from "../src/browser-media-device";
+
+let browserMediaDevices = new BrowserMediaDevice();
+
+browserMediaDevices.getAudioInputList()
+    .then(audioInputDevices => {
+        console.log("detected audio devices", audioInputDevices);
+        /*
+            assuming the detected devices are:
+            audioinput: default (Built-in Microphone) id=RKxXByjnabbADGQNNZqLVLdmXlS0YkETYCIbg+XxnvM=
+            audioinput: Built-in Microphone id=r2/xw1xUPIyZunfV1lGrKOma5wTOvCkWfZ368XCndm0=
+         */
+
+        let builtInMicrophone = audioInputDevices[1]; // built-in mic
+        browserMediaDevices.setAudioInput(builtInMicrophone); // store built-in mic
+    });
+
+let startMicRecording = () => {
+    let baseConstraints = {
+        audio: true,
+        video: false
+    };
+
+    let preferredDeviceConstraints = browserMediaDevices.getDeviceConstraints(baseConstraints);
+
+    // start recording
+    navigator.mediaDevices
+        .getUserMedia(preferredDeviceConstraints)
+        .then(stream => {
+            console.log("Media stream", stream);
+
+            // Audio context / processing here
+        });
+};
